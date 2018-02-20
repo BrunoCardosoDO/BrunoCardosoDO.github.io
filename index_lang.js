7b1ff6947d1d5ec0650fc980fe28a6472f6591c5
Bruno Cardoso <brunocardosodo@gmail.com>
2018-02-20 14:19:03 -0300

    Naive attempt at java scripting N.3

--- index_lang.js
+++ index_lang.js
@@ -1,10 +1,10 @@
 function setLang(){
 	switch (location.hash.substr(1)){
 		case "l=1":
-			document.getElementById('pt-BR').style.display='inline';
+			document.getElementById('en-US').style.display='inline';
 			break;
 		case "l=2":
-			document.getElementById('en-US').style.display='inline';
+			document.getElementById('pt-BR').style.display='inline';
 			break;
 		default:
 			document.getElementById('pt-BR').style.display='inline';
