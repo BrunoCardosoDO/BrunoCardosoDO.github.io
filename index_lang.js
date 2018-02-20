a5bf7b73544c5fa330ae3ddfc0ee268df1a57ad9
Bruno Cardoso <brunocardosodo@gmail.com>
2018-02-20 14:15:03 -0300

    Naive attempt at java scripting N.2

--- index_lang.js
+++ index_lang.js
@@ -1,13 +1,13 @@
 function setLang(){
 	switch (location.hash.substr(1)){
 		case "l=1":
-			document.getElementById("pt-BR").style.display='none';
+			document.getElementById('pt-BR').style.display='inline';
 			break;
 		case "l=2":
-			document.getElementById("en-US").style.display='none';
+			document.getElementById('en-US').style.display='inline';
 			break;
 		default:
-			document.getElementById("pt-BR").style.display='none';
+			document.getElementById('pt-BR').style.display='inline';
 			break;
 	}
 }
