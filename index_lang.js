13cde5351d7113dfb61d1e0de4f0903c6f6bdc69
Bruno Cardoso <brunocardosodo@gmail.com>
2018-02-20 13:49:10 -0300

    Finally done with language stuff (I guess)

--- index_lang.js
+++ index_lang.js
@@ -1,12 +1,14 @@
-var lang = location.hash.substr(1)
-switch (lang){
-	case "l=1":
-		document.getElementById("pt-BR").style.display='none';
-		break;
-	case "l=2":
-		document.getElementById("en-US").style.display='none';
-		break;
-	default:
-		document.getElementById("pt-BR").style.display='none';
-		break;
+function setLang()
+{
+	var lang = location.hash.substr(1)
+	switch (lang){
+		case "l=1":
+			document.getElementById("pt-BR").style.display='none';
+			break;
+		case "l=2":
+			document.getElementById("en-US").style.display='none';
+			break;
+		default:
+			document.getElementById("pt-BR").style.display='none';
+			break;
 }
